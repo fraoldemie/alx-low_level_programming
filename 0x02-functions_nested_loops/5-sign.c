4c88d32f40f103eb5743293809516ee0332053ae
fraoldemie <fraoldemie@gmail.com>
2022-04-12 15:15:10 -0700

    ew

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,7 +17,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar('48');
+		_putchar(48);
 		return(0);
 	}
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_last_digit returns the last digit of a number.
+ *
+ */
+int print_last_digit(int n)
+{
+	return(n % 10);
+}
