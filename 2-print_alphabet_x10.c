78f91d3a898a586cafcf62575899f3ad4ebd3b26
fraoldemie <fraoldemie@gmail.com>
2022-04-12 14:06:25 -0700

    c

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase, 10 times, followed by a new line.
+ *Return : Always return 0
+ */
+void print_alphabet_10x(void)
+{
+	int i;
+	char ch;
+
+	i = 0;
+
+	while (i < 10)
+	{
+
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	        _putchar('\n');
+		i++;
+	}
+}
+
