9fb826952c2d7756e637d7037827adadc64a19be
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-15 22:47:00 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,9 +13,13 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	while (n > 0)
+	/*while (n > 0)
 	{
 		_putchar (n % 10);
 		n = n / 10;
 	}
+	*/
+	if (n / 10)
+		print( n / 10);
+	putchar(n % 10 + '0');
 }
