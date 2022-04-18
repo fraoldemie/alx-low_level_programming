70b917477a0dcdb3d23a2b164859c4cd227aae85
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-18 17:24:49 +0300

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values strored at two given pointers
+ * @a: first integer
+ * @b: second integer
+ * Return: (0)
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	
+	c = *a;
+	*a = *b;
+	*b = c;
+}
