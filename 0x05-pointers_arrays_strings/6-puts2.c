4d7b635af61ef9fc3420f6a04f21372eb592922f
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-19 17:34:05 +0300

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * puts2 - prints every other character in astring
+ *
+ * @s: the input string
+ *
+ * Return: (0)
+ */
+void puts2(char *s)
+{
+	for(int i = 0; *(s+i) != '\0'; i = i + 2)
+	{
+		putchar(*(s+i));
+	}
+}
