bdb6c860e7f70f1abd48ee0a1581e018fcd0971c
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-20 01:21:20 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,11 @@ void puts_half(char *str)
 	int i;
 	int len = 0;
 
-	for(i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		len++;
 	}
-	for (i = ((len - 1)/2) + 1; str[i] != '\0'; i++)
+	for (i = ((len - 1) / 2) + 1; str[i] != '\0'; i++)
 	{
 		putchar(str[i]);
 	}
