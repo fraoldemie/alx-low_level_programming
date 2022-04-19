01efffa43e5b772e30f367ee12ac9f0328bfbd8e
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-19 11:59:09 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdout
+ *
+ * @str: the pointer pointing to where the string to be printed is located
+ *
+ * Return: (0)
+ */
+void _puts(char *str)
+{
+	int i;
+	
+	for( i = 0; *(str+i) == '0\'; i++)
+	    {
+		    putchar(*(str+i));
+	    }
+	    putchar('\n');
+}
+	    
