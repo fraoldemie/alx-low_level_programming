fa8a80f84954942a6b37ed3f586515a5a2380984
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-20 01:13:28 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,9 @@
  *
  * Return: the pointer to dest
  */
-char *_strcpy(char *dest, char* src)
+char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	
 	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
