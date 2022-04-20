e52bc3fcc7f69496d2868b96850b804f133142eb
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-21 00:04:36 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -21,7 +21,7 @@ char * _strcat(char *dest, char *src)
 	}
 	while (*(src + j) != '\0')
 	{
-		*(dest + len + j + 1) = *(src + j);
+		*(dest + len + j - 1) = *(src + j);
 		j++;
 	}
 	*(dest + len + j) = '\0';
