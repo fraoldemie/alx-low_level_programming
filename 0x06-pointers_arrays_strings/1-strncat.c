fe25e6674d49bd27175893cca4128e9f576b4764
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-21 14:55:30 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,32 @@
+#include "main.h"
+#include "stdio.h"
 
+/**
+ * _strncat - copys a given number of characters from one string to the other
+ *
+ * @dest: the destination string
+ *
+ * @src: the source string
+ *
+ * @n: the number of characters to be copied
+ *
+ * Return: pointer to the final string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		len2++;
+	}
+	char result[len + len2 + 1];
+	for (i = 0; i
+	for (i = 0; i < n; i++)
+	{
+		result[len + i] = src[i];
+	}
+	
+   
