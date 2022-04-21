99e2f52ff76987bfa065e93f434733d1eef79da0
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-21 20:25:08 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,14 +16,23 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j, len;
 	
+	j = 0;
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		len++;
 	}
-	for (j = 0; j < n; j++)
+	while (j < n && src[j] != '\0')
 	{
 		dest[len] = src[j];
 		len++;
+		j++
+	}
+	if (src[j] == '\0')
+	{
+		while (len < n)
+		{
+			dest[len] = '\0';
+		}
 	}
 	return (dest);
 }
