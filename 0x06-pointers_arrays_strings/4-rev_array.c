2a350aaaf1582e83d60b1caf80a315d2902fec23
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-21 16:40:54 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,2 +1,26 @@
 #include "main.h"
-#include 
+#include "stdio.h"
+
+/**
+ * rev-array - reverses a given array
+ *
+ * @a: the array to be reversed
+ *
+ * @n: the length of the arrray
+ *
+ * Return: (0)
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+    	int tmp;
+    
+	for (i = 0; i <= n/2; i++)
+	{
+	    tmp = a[i];
+	    a[i] = a[n - 1 - i];
+	    a[n - 1 - i] = tmp;
+	}
+}
+  
+  
