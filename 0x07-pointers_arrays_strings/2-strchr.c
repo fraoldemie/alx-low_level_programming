2a8761c9ab51a9406c2240931713ca04f626ae75
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-25 11:05:26 +0300

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strchr - Returns a pointer to the first occurrence of the character c
+ *
+ * @s: the string to be searched
+ *
+ * @c: the character to be searched
+ *
+ * Return: pointer to the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	int R = 0;
+	int *C;
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			R = 1;
+			C = &c;
+			break;
+		}
+	}
+	if (R == 1)
+		return (C);
+	return (NULL);
+}
