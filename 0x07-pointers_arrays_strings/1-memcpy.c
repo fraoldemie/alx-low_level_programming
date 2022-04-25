ee80600041d915e38ea972400dcdf6082cabdd1f
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-25 10:24:05 +0300

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - copies a given number of bytes from one memory area to another
+ *
+ * @sr: a pointer to the source memory area
+ *
+ * @dest: a pointer to the destination memory area
+ *
+ * @n: the number of bytes to be copied
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+		
+	
+ 
