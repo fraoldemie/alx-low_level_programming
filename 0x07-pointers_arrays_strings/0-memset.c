7ae5ce9a34bf582fe5f8b0a2f547fda51f9dce3c
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-25 10:08:18 +0300

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fills up a given number of memory locations with a given pointer
+ *
+ * @s: the starting location of the memory adresses to be filled
+ *
+ * @b: the character whose pointer we use to fill up the adresses
+ *
+ * @n: the number of memory locations to be filled
+ *
+ * Return: the pointer to the filled up memory locations
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int *B = &b;
+	
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = B;
+	}
+	return (s);
+}
