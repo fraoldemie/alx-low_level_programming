e5616fc7cf42e9e80a6ecf0fe050c6517f2b148c
fraoldemie <101448372+fraoldemie@users.noreply.github.com>
2022-04-26 21:31:08 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,32 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * strpbrk - locates the first occurence of any character from a given string in another string
- * @s: the string to be searched
- * @accept: the string whose members are to be searched 
- * Returns a pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
+ * _strpbrk - prints the consecutive caracters of s1 that are in s2.
+ * @s: source string
+ * @accept: searching string
+ *
+ * Return: new string.
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-	int j;
-	char *C;
-	
-	int R = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	unsigned int i, j;
+
+	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; *(accept + j); j++)
 		{
-			if (s[i] == accept[j])
+			if (*(s + i) == *(accept + j))
 			{
-				R = 1;
-				C = &(s[i]);
 				break;
 			}
 		}
-		if (R == 1) 
-			break;
+		if (*(accept + j) != '\0')
+		{
+			return (s + i);
+		}
 	}
-	if (R == 1)
-			return (C);
-		return (0);
+	return (0);
 }
